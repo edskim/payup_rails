21f15b99248c5290036f2ed846a3f56c7d2e6e1e
Eddie <edskim@gmail.com>
2012-10-02 00:01:00 -0700

    Fix a bug

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,7 +1,7 @@
 require 'twilio-ruby'
+require 'auth_token.rb'
 
 Account_sid = 'AC49f6dc9e9c193f147a29c6f127f911bb'
-Auth_token = 'de4759863ca89f7e9ae0588639e34a25'
 
 class CallsController < ApplicationController
   respond_to :json
@@ -12,11 +12,12 @@ class CallsController < ApplicationController
   end
 
   def create
+    puts "Auth token is " + ::Auth_token
     @call = Call.new(params[:call])
     if @call.save
       @client = Twilio::REST::Client.new(::Account_sid, ::Auth_token)
       @account = @client.account
-      @call = @account.calls.create({:from => '+18183517966', :to => "+1#{@call.number}", :url => "http://afternoon-waters-4041.herokuapp.com/calls.xml?requester=#{@call.requester}&name=#{@call.name}&amount=#{@call.amount}&date=#{@call.date}", :method => 'GET'})
+      @call = @account.calls.create({:from => '+18183517966', :to => "+1#{@call.number}", :url => "http://afternoon-waters-4041.herokuapp.com/twiml.xml?requester=#{@call.requester}&name=#{@call.name}&amount=#{@call.amount}&date=#{@call.date}", :method => 'GET'})
     end
     respond_with(@call)
   end
